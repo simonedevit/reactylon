bfbfad79d2fe1d8427ed9215b692eae405774b3c
Simone De Vittorio <simonedevittorio.me@gmail.com>
2024-11-30 12:51:33 +0100

    chore(general): update licences

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,6 +10,15 @@ cp ../../.npmrc ./build/.npmrc
 # copy the npmignore file to exclude stuff from publishing
 cp .npmignore ./build/.npmignore
 
+COPYRIGHT_TEXT="/* Copyright (c) 2024 Simone De Vittorio. */"
+FILE="./build/index.js"
+
+# add the copyright text at the top of index.js
+echo -e "$COPYRIGHT_TEXT\n$(cat $FILE)" > $FILE
+
+## add licenses
+cp "../../LICENSE.md" "./build/index.js.LICENSE.txt"
+
 cd build
 
 npm publish
