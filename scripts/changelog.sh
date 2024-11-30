5ebb1a5d847ff488b3f39696d6fcb478c70b36e1
Simone De Vittorio <simonedevittorio.me@gmail.com>
2024-11-30 16:16:30 +0100

    fix(general): copy CHANGELOG.md on root

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,5 +1,5 @@
 # copy CHANGELOG.md file on root (monorepo)
 cp CHANGELOG.md ../../CHANGELOG.md
 
-git add CHANGELOG.md
+git add ../../CHANGELOG.md
 git commit -m "docs(general): copy CHANGELOG.md on root"
