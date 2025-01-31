77cf3590bd75e513de91bad8e074d40449c212fd
Simone De Vittorio <simonedevittorio.me@gmail.com>
2025-01-31 21:40:57 +0100

    test(general): mock react-dom/server

--- packages/library/setup-jest.js
+++ packages/library/setup-jest.js
@@ -1,12 +1,19 @@
-// suppress warning React component capital letter
+import { jest } from '@jest/globals';
 
-const originalError = console.error.bind(console.error)
+// Mock react-dom/server because Jest is executed in jsdom test environment
+jest.mock('react-dom/server', () => {
+    return {
+        renderToStaticMarkup: jest.fn(),
+    };
+});
+
+// Suppress warning React component capital letter (Babylon.js elements are JSXIntrinsicElements)
+const originalError = console.error.bind(console.error);
 
 beforeAll(() => {
-    console.error = (msg, ...rest) =>
-        !msg.toString().includes('If you meant to render a React component, start its name with an uppercase letter.') && originalError(msg, ...rest);
-})
+    console.error = (msg, ...rest) => !msg.toString().includes('If you meant to render a React component, start its name with an uppercase letter.') && originalError(msg, ...rest);
+});
 
 afterAll(() => {
-    console.error = originalError
-})
+    console.error = originalError;
+});
